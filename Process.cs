991b7ca9f82cd8548c76fb65e3cfae57d258c282
Keduvar <rentqaz15@gmail.com>
2023-07-14 10:00:24 +0300

    init: add entity process

---
+++ Process.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ASE.PodISMConsole
+{
+    public class Process
+    {
+        public int Id { get; set; }
+
+        public int Up_id { get; set; }
+
+        public string Title { get; set; }
+
+        public string OwnerGroupGrocess { get; set; }  
+
+        public string Methodologist { get; set; }
+
+        public string GeneralInfoName { get; set; }
+
+        public string DistributionArea { get; set; }
+
+        public string JustificationOrder { get; set; }
+
+        public string ProcessGroupMetrics { get; set; }
+
+        public string LinkProcessMap { get; set; }
+
+        public string Link {get; set;}
+    }
+}
