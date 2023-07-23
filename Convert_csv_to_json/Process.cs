e3a15ac949d1a97caf630e2c06bd5a4471311e67
Keduvar <rentqaz15@gmail.com>
2023-07-23 16:18:39 +0300

    refactor: entity process

--- Convert_csv_to_json/Process.cs
+++ Convert_csv_to_json/Process.cs
@@ -2,28 +2,39 @@ using System.Text.Json.Serialization;
 
 namespace ASE.PodISMConsole
 {
-    public class Process 
+    public class Process
     {
+        [JsonPropertyName("UID")]
         public string UID { get; set; }
 
+        [JsonPropertyName("UpUID")]
         public string UpUID { get; set; }
 
+        [JsonPropertyName("Title")]
         public string Title { get; set; }
 
-        public string EmpParentProcess { get; set; }
-
-        public string EmpDevBy { get; set; }
-
-        public string GeneralInfoName { get; set; }
-
-        public string DistributionArea { get; set; }
-
-        public string JustificationOrder { get; set; }
-
-        public string LinkProcessMap { get; set; }
-
+        [JsonPropertyName("Link")]
         public string Link { get; set; }
 
+        [JsonPropertyName("EmpParentProcess")]
+        public string EmpParentProcess { get; set; }
+
+        [JsonPropertyName("EmpDevBy")]
+        public string EmpDevBy { get; set; }
+
+        [JsonPropertyName("GeneralInfoName")]
+        public string GeneralInfoName { get; set; }
+
+        [JsonPropertyName("DistributionArea")]
+        public string DistributionArea { get; set; }
+
+        [JsonPropertyName("JustificationOrder")]
+        public string JustificationOrder { get; set; }
+
+        [JsonPropertyName("LinkProcessMap")]
+        public string LinkProcessMap { get; set; }
+
+        [JsonPropertyName("Chields")]
         public List<Process> Chields { get; set; }
     }
 }
