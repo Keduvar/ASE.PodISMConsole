3085f2c8440365c80e42b9181feff71fa93827ef
Keduvar <rentqaz15@gmail.com>
2023-07-26 11:20:30 +0300

    refactor: change structure convert_json_to_csv
    *add NUnit

---
+++ Convert_json/Convert_json_to_csv.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Convert_json_to_csv.Tests
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
